8643a599a572819311fd8ddc704a9ccbc8212f04
MikaIce <michael.benoit13@gmail.com>
2021-05-15 01:57:06 -0300

    DOC + PEP8

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.7
 # -*-coding:utf-8 -
+"""File launching"""
 
 from menu import MenuHandler
 from DATA.datamanager import DataManager
@@ -7,19 +8,17 @@ from SQL.dbcreator import DBCreator
 
 def main():
 
-    # Initialize the DB when launching the program
+    """Initialize the DB when launching the program"""
     create_db = DBCreator()
     create_db.create_user()
     create_db.create_db()
     create_db.quit_database()
 
-    # Fill in The DB just created with data from OpenFoodFacts
     initialize_data = DataManager()
     initialize_data.create_tables()
     initialize_data.insert_data()
     initialize_data.quit_database()
 
-    # Launch the menu to interact with the program
     display_menu = MenuHandler()
     display_menu.show_main_menu()
 
